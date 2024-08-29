bb7b8df7d6ff8cda4563f20b86ddee8aab3e5851
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-08-29 08:25:56 +0530

    Task-7 colocation files

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFounds() {
   return (
     <div>
-      <h1>Page not foound!</h1>
+      <h1>Page not found!</h1>
       <p>Could not find requested resource.</p>
     </div>
   );
