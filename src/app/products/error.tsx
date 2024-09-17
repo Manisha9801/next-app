c8522bc8ba80ec3f8b56cf514d8b446356302e3a
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-09-17 06:53:30 +0530

    Task-19 Error bubling and handling for nested Layout component.

---
+++ src/app/products/error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void; //excecuting reset function attempts to re-render the error boundaries contents
+}) {
+  return (
+    <div>
+      {error.message}
+      <br />
+      <br />
+      <button onClick={reset}>Try again</button>{" "}
+      {/*Here reset function re-renders the review ID component */}
+    </div>
+  );
+}
