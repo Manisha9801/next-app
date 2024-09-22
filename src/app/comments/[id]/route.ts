96aee77bf4dae7aed233a945801bc3167d08df95
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-09-22 11:44:29 +0530

    Task-27 PATCH request demo.

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -10,3 +10,16 @@ export async function GET(
   );
   return NextResponse.json(comment);
 }
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return NextResponse.json(comments[index]);
+}
