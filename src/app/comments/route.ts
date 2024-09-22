ad285b240d4e1c5f369f05f788f9abf18cedb508
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-09-22 09:51:13 +0530

    Task-26 Route Handler's POST request demo.

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,6 +1,21 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { comments } from "./data";
 
 export async function GET() {
-  return NextResponse.json(comments);
+  return NextResponse.json(comments);  
+}
+
+export async function POST(request: NextRequest) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201   //201 reflects new resource creation.
+  });
 }
