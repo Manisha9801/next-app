5e54793aadf4d9bfbc240661f2af8a21a6d7ea6c
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-09-23 19:45:45 +0530

    Task-31 Middleware using conditional statements for redirection and re-writing of URL demo.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,16 @@
 
 import { NextResponse, NextRequest } from "next/server";
 
-export function middleware(request : NextRequest){
-    return NextResponse.redirect(new URL("/", request.url))
+export function middleware(request: NextRequest) {  //Function name should me middleware
+  //Approach 1 - Custome Matcher Config
+  // return NextResponse.redirect(new URL("/", request.url))
+  //Approach 2 - Conditional Statements
+  if (request.nextUrl.pathname === "/profile") {
+    // return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.rewrite(new URL("/dashboard", request.url));
+  }
 }
 
-export const config = {
-    matcher : "/profile"
-}
+// export const config = {
+//     matcher : "/profile"
+// }
