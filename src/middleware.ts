607d484ac5833d80131bb569e4d3f69c17f538c8
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2024-09-23 19:51:07 +0530

    Task-32 Middleware using cookies demo.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,27 @@
 
 import { NextResponse, NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {  //Function name should me middleware
-  //Approach 1 - Custome Matcher Config
-  // return NextResponse.redirect(new URL("/", request.url))
-  //Approach 2 - Conditional Statements
-  if (request.nextUrl.pathname === "/profile") {
-    // return NextResponse.redirect(new URL("/dashboard", request.url));
-    return NextResponse.rewrite(new URL("/dashboard", request.url));
-  }
-}
+// export function middleware(request: NextRequest) {  //Function name should me middleware
+//   //Approach 1 - Custome Matcher Config
+//   // return NextResponse.redirect(new URL("/", request.url))
+//   //Approach 2 - Conditional Statements
+//   if (request.nextUrl.pathname === "/profile") {
+//     // return NextResponse.redirect(new URL("/dashboard", request.url));
+//     return NextResponse.rewrite(new URL("/dashboard", request.url));
+//   }
+// }
 
 // export const config = {
 //     matcher : "/profile"
 // }
+
+
+//Modifying middlewares to handle user preferences for themes and add a custom header for all responses.
+export function middleware(request : NextRequest){
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if(!themePreference){
+    response.cookies.set("theme", "dark");
+  }
+  return response;
+}
